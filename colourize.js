78fded05852dcc42967b7cfae43a95574d16c0b0
William Villeneuve <ciiqr@users.noreply.github.com>
2021-06-05 13:39:10 -0400

    initial commit

---
+++ colourize.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const dark = document.body.querySelector('#dark');
+    const light = document.body.querySelector('#light');
+    const editor = document.body.querySelector('#editor');
+    const renderer = document.body.querySelector('#renderer');
+    const content = document.body.querySelector('#content');
+
+    // re-render on input
+    editor.addEventListener('input', function(e) {
+        // clear existing rendering
+        renderer.innerHTML = "";
+
+        // split into sections with and without hex codes
+        const sections = e.target.value.match(/(#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\b|[^#]+)/g);
+        for (const section of sections) {
+            const sectionElem = document.createElement('span');
+            sectionElem.innerHTML = section;
+
+            // set colour to hex code
+            if (section.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/g)) {
+                sectionElem.classList.add('colour-bubble');
+                sectionElem.style = `
+                    color: ${section};
+                    border-color: ${section};
+                `;
+            }
+
+            // render section
+            renderer.appendChild(sectionElem);
+        }
+    });
+
+    // toggle dark/light
+    dark.addEventListener('click', function(e) {
+        // dark mode
+        // TODO: should do with class
+        content.style = `
+            background-color: #272822;
+            color: white;
+        `;
+    });
+    light.addEventListener('click', function(e) {
+        // reset to default
+        content.style = '';
+    });
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# hex-colourizer
+
+paste/type in some text with hex codes in it, it will render the text with those hex codes colourized
